e9b72648c19ba8165094e50e3f102d416d7fe8bb
CLDXiang <417198670@qq.com>
2020-04-25 21:03:43 +0800

    init todo-mvc-basic

---
+++ todo-mvc-basic/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}

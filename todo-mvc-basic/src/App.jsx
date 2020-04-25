c0e7c085023f1c5f53ae714c5ccd658fce8394ef
CLDXiang <417198670@qq.com>
2020-04-25 22:51:27 +0800

    introduce eslint-airbnb & fix main layout

--- todo-mvc-basic/src/App.jsx
+++ todo-mvc-basic/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
+import './App.css';
 
 export default function App() {
   return (
     <Router>
-      <div>
+      <div className="App">
         <Switch>
           <Route exact path="/">
             <Home />

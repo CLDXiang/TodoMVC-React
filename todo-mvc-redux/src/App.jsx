7c2450e975282834e4a42d288fb97520d581c309
CLDXiang <417198670@qq.com>
2020-04-29 17:53:30 +0800

    import redux

--- todo-mvc-redux/src/App.jsx
+++ todo-mvc-redux/src/App.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import appReducer from './reducers/index';
 import Home from './components/Home';
 import './App.css';
 
+const store = createStore(appReducer);
+
 export default function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }

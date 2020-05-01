bafb4f8b02d1893704e2348a88eb72eff2a569f1
CLDXiang <417198670@qq.com>
2020-05-01 21:36:46 +0800

    organize all errors response in an independent file

---
+++ api.todo-mvc/services/responseErrors.js
@@ -0,0 +1,6 @@
+export default {
+  1: { errCode: 1, errMsg: 'invalid request body fields!' },
+  2: { errCode: 2, errMsg: 'existed username!' },
+  3: { errCode: 3, errMsg: 'wrong username!' },
+  4: { errCode: 4, errMsg: 'wrong password!' },
+}
--- api.todo-mvc/services/service.user.js
+++ api.todo-mvc/services/service.user.js
@@ -1,16 +1,17 @@
 import { userModel } from '../storage';
+import err from './responseErrors';
 
 export const userRegister = async (ctx) => {
   const { username, password } = ctx.request.body;
   if (username && password) {
     const user = await userModel.findOne({ username }).exec();
     if (user) {
-      ctx.response.body = { errCode: 2, errMsg: 'existed username!' };
+      ctx.response.body = err[2];
     } else {
       ctx.response.body = await new userModel({ username, password }).save();
     }
   } else {
-    ctx.response.body = { errCode: 1, errMsg: 'invalid username or password!' };
+    ctx.response.body = err[1];
   }
 };
 
@@ -19,13 +20,13 @@ export const userLogin = async (ctx) => {
   if (username && password) {
     const user = await userModel.findOne({ username }).exec();
     if (!user) {
-      ctx.response.body = { errCode: 3, errMsg: 'wrong username!' };
+      ctx.response.body = err[3];
     } else if (user.password !== password) {
-      ctx.response.body = { errCode: 4, errMsg: 'wrong password!' };
+      ctx.response.body = err[4];
     } else {
       ctx.response.body = { success: true, id: user._id };
     }
   } else {
-    ctx.response.body = { errCode: 1, errMsg: 'invalid username or password!' };
+    ctx.response.body = err[1];
   }
 }

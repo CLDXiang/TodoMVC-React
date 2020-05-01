a8665eea22e047b00caed9a37833d003c6335a6a
CLDXiang <417198670@qq.com>
2020-05-01 21:53:05 +0800

    finish basic todo api

--- api.todo-mvc/services/service.todo.js
+++ api.todo-mvc/services/service.todo.js
@@ -1,17 +1,53 @@
 import { todoModel } from '../storage';
+import err from './responseErrors';
 
 export const todoGet = async (ctx) => {
   const { userId } = ctx.request.body;
+  if (userId) {
+    ctx.response.body = await todoModel
+      .find()
+      .where('creatorId')
+      .equals(userId)
+      .sort('createdAt')
+      .exec();
+  } else {
+    ctx.response.body = err[1];
+  }
 };
 
 export const todoAdd = async (ctx) => {
   const { userId, content, deadline } = ctx.request.body;
+  if (userId && content && deadline) {
+    ctx.response.body = await new todoModel({
+      creatorId: userId,
+      content,
+      deadline,
+    }).save();
+  } else {
+    ctx.response.body = err[1];
+  }
 };
 
 export const todoRemove = async (ctx) => {
   const { userId, todoId } = ctx.request.body;
+  if (userId && todoId) {
+    ctx.response.body = await todoModel.deleteOne({
+      _id: todoId,
+      creatorId: userId,
+    });
+  } else {
+    ctx.response.body = err[1];
+  }
 };
 
 export const todoEdit = async (ctx) => {
   const { userId, todoId, content, deadline } = ctx.request.body;
+  if (userId && todoId && content && deadline) {
+    ctx.response.body = await todoModel.updateOne(
+      { _id: todoId, creatorId: userId },
+      { content, deadline }
+    );
+  } else {
+    ctx.response.body = err[1];
+  }
 };

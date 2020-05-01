65ff5a7a53beb9b51199711fe83b4851fb734254
CLDXiang <417198670@qq.com>
2020-05-01 23:36:47 +0800

    minor

--- todo-mvc-hooks/src/App.jsx
+++ todo-mvc-hooks/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Switch, Route, Redirect,
+} from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import './App.css';
